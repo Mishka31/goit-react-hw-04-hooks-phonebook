40e0480ca2cacc01c62f533ae22547be929627ca
Misha Krasnonos <muzishutka7@gmail.com>
2021-09-13 21:02:00 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ContactForm from "./Components/ContactForm/ContactForm.jsx";
+import ContactList from "./Components/ContactList/ContactList.jsx";
+import Filter from "./Components/Filter/Filter.jsx";
+import s from "./App.module.css";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([{ id: "1", name: "Misha Krasnonos", number: "050-762-05-64" }]);
+  const [filter, setfilter] = useState("");
+
+  const lowerFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(lowerFilter));
+
+  const onFind = (e) => setfilter(e.target.value);
+  const formSubmitHandler = (data) => setContacts([...contacts, { id: uuidv4(), ...data }]);
+  const onDelete = (id) => setContacts((contacts) => contacts.filter((contact) => contact.id !== id.target.id));
+
+  useEffect(() => {
+    const contactsStorage = JSON.parse(localStorage.getItem("contacts"));
+    if (contactsStorage) {
+      setContacts(contactsStorage);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={s.titleH1}>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} listArrey={contacts} />
+      <h2 className={s.titleH2}>Contacts</h2>
+      <Filter value={filter} onSearch={onFind} />
+      <ContactList contacts={filteredContacts} onDelete={onDelete} />
     </div>
   );
-}
+};
 
 export default App;
